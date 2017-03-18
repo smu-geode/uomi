d575009d3bf5313d88d3195c3ae52874fec3bd77
phrz <phrz@me.com>
2017-03-18 15:19:00 -0500

    Working Docker Compose setup for Caddy+PHP

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+
+var paths = {
+    html: 'src/public/*.html',
+	img: 'src/public/img/*.*',
+    scss: 'src/public/scss/*.scss',
+    js: 'src/public/js/*.js',
+	php: 'src/*.php'
+};
+
+var buildPaths = {
+    html: 'build/public/',
+	img: 'build/public/img/',
+    css: 'build/public/css/',
+    js: 'build/public/js/',
+	php: 'build/'
+};
+
+/*************************************/
+/* HTML                              */
+/*************************************/
+
+gulp.task('html', function() {
+    return gulp.src(paths.html)
+        .pipe(gulp.dest(buildPaths.html));
+});
+
+gulp.task('html:watch', function() {
+    gulp.watch(paths.html, ['html']);
+});
+
+/*************************************/
+/* Images                            */
+/*************************************/
+
+gulp.task('img', function() {
+    return gulp.src(paths.img)
+		.pipe(imagemin())
+        .pipe(gulp.dest(buildPaths.img));
+});
+
+gulp.task('img:watch', function() {
+    gulp.watch(paths.img, ['img']);
+});
+
+/*************************************/
+/* SASS                              */
+/*************************************/
+
+gulp.task('sass', function() {
+    return gulp.src(paths.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(buildPaths.css));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch(paths.scss, ['sass']);
+});
+
+/*************************************/
+/* Javascript                        */
+/*************************************/
+
+gulp.task('js', function() {
+    return gulp.src(paths.js)
+        .pipe(gulp.dest(buildPaths.js));
+});
+
+gulp.task('js:watch', function() {
+    gulp.watch(paths.js, ['js']);
+});
+
+/*************************************/
+/* PHP                               */
+/*************************************/
+
+gulp.task('php', function() {
+    return gulp.src(paths.php)
+        .pipe(gulp.dest(buildPaths.php));
+});
+
+gulp.task('php:watch', function() {
+    gulp.watch(paths.php, ['php']);
+});
+
+/*************************************/
+/* General                           */
+/*************************************/
+
+gulp.task('clean', function() {
+    return del(['build/*']);
+});
+
+gulp.task('watch', ['html:watch','img:watch','sass:watch','js:watch','php:watch']);
+
+gulp.task('default', ['html','img','sass','js','php']);
