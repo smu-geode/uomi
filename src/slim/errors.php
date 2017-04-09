64101585382239ff2f6227d9306b7e1ec3acca19
phrz <phrz@me.com>
2017-04-09 15:03:22 -0500

    Important error handler changes and namespace fixes

--- src/slim/errors.php
+++ src/slim/errors.php
@@ -2,46 +2,41 @@
 
 namespace Uomi;
 
+function genericErrorHandler(string $header, \Slim\Container $c) {
+	return function ($request, $response, $exception) use ($c, $header) {
+
+		$c->get('logger')->error("[$header] ".$exception->getMessage());
+
+		$info = [
+			'type' => get_class($exception),
+			'errorMessage' => $exception->getMessage(),
+			'line' => $exception->getLine(),
+			'file' => $exception->getFile(),
+			'trace' => $exception->getTrace()
+		];
+
+		$c->get('logger')->error("[$header] ".json_encode($info));
+
+		$data = new \stdClass();
+		if($c->get('settings')['debug']) {
+			$data = $info;
+		}
+
+		$stat = new \Uomi\Status($data);
+		$stat = $stat->error("ObfuscatedInternalServerError")
+					 ->message("Something went wrong on our side!");
+		return $response->withStatus(500)->withJson($info);
+	};
+}
+
 function registerErrorHandlers(\Slim\Container $c) {
 	// Exceptions get sent through this handler
 	$c['errorHandler'] = function ($c) {
-	    return function ($request, $response, $exception) use ($c) {
-	        $c->get('logger')->error("[Exception] ".$exception->getMessage());
-	        $stat = new \Uomi\StatusContainer();
-	        $stat->error("ObfuscatedInternalServerError");
-	        $stat->message("Something went wrong on our side!");
-	        if($c->get('settings')['debug']) {
-	            $stat->data = [
-	                'type' => get_class($exception),
-	                'errorMessage' => $exception->getMessage(),
-	                'line' => $exception->getLine(),
-	                'file' => $exception->getFile(),
-	                'trace' => $exception->getTrace()
-	            ];
-	        }
-	        return $response->withStatus(500)
-	                        ->withJson($stat);
-	    };
+		return genericErrorHandler('Exception', $c);
 	};
 	// PHP 7 Error objects get sent through this handler
 	$c['phpErrorHandler'] = function ($c) {
-	    return function ($request, $response, $exception) use ($c) {
-	        $c->get('logger')->error("[PHP Error] ".$exception->getMessage());
-	        $stat = new \Uomi\StatusContainer();
-	        $stat->error("ObfuscatedInternalServerError");
-	        $stat->message("Something went wrong on our side!");
-	        if($c->get('settings')['debug']) {
-	            $stat->data = [
-	                'type' => get_class($exception),
-	                'errorMessage' => $exception->getMessage(),
-	                'line' => $exception->getLine(),
-	                'file' => $exception->getFile(),
-	                'trace' => $exception->getTrace()
-	            ];
-	        }
-	        return $response->withStatus(500)
-	                        ->withJson($stat);
-	    };
+		return genericErrorHandler('PHP Error', $c);
 	};
 
 }
