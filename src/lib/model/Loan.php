31c55df383a15d85806e56987d4f6b4aee04efb7
jakerowland@verizon.net <jakerowland@verizon.net>
2017-04-09 21:31:04 -0500

    Started working on the Loan model. Have no idea if correct tho :/

--- src/lib/model/Loan.php
+++ src/lib/model/Loan.php
@@ -2,5 +2,18 @@
 namespace Uomi\Model;
 
 class Loan extends \Illuminate\Database\Eloquent\Model {
+	
+	protected $cast = [
+		'confirmed' => 'boolean',
+	];
 
+	public function users() {
+		return $this->hasMany('Uomi\User','id');
+	}
+
+	public function payments() {
+		return $this->hasMany('Uomi\Payment','loan_id');
+	}
+
+	protected $dates = ['created_at', 'confirmed_at', 'completed_at'];
 }
