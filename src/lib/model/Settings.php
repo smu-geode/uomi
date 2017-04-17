11378908b675b3be79f06ecf42f4bab7dc8a3255
Firehex <caldridge@smu.edu>
2017-04-17 16:39:28 -0500

    added putUserSettings functionality

--- src/lib/model/Settings.php
+++ src/lib/model/Settings.php
@@ -5,4 +5,5 @@ class Settings extends \Illuminate\Database\Eloquent\Model {
 		return $this->belongsTo('Uomi\User','user_id');
 	}
 	//protected $settings = ['allNotifications', 'borrowingRequests', 'payBackReminders', 'viewEmail'];
+	protected $hidden = ['user_id', 'created_at', 'updated_at'];
 }
