5572aa16bec73b6d8753cf109b699bf3d2682e95
phrz <phrz@me.com>
2017-04-25 09:19:53 -0500

    Fix again

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Taking down "uomi_${GULP_TARGET}"
+echo "Taking down 'uomi_${GULP_TARGET}'"
 docker-compose -f ./config/dev/docker-compose.yml -p "uomi_${GULP_TARGET}" down --rmi local
