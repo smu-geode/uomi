686fd0d7cbc6c003a2652e7657922ca26f80ca0e
Jake Carlson <jrcarlson@smu.edu>
2017-04-26 22:08:12 -0500

    added amount validation and convert to cents

--- src/public/app/borrow-form/borrow-form.component.ts
+++ src/public/app/borrow-form/borrow-form.component.ts
@@ -45,29 +45,49 @@ export class BorrowFormComponent implements OnInit {
 
 	completeBorrow() {
 		// convert amount string to cents - !!
-		this.newLoan.amountCents = +this.amount;
+		if (this.isValidCurrenyString(this.amount)) {
+			this.newLoan.amountCents = this.convertToCents(this.amount);
+			console.log(this.newLoan.amountCents);
+			
+			this.newLoan.to = sessionStorage.user_id;
 
-		this.newLoan.to = sessionStorage.user_id;
-
-		// get user id for toUser
-		let emailMatch: boolean = false;
-		this.usersService.searchUserByEmail(""+this.fromUser)
-		.subscribe(x => {
-			console.log(x);
-			if (x[0].email == this.fromUser) {
-				this.newLoan.from = x[0].id;
-				this.loansService.postNewLoan(+this.newLoan.from, +this.newLoan.to, 
-					this.newLoan.amountCents, +this.newLoan.category)
-					.subscribe(x => this.cancel(), x => console.log(x));
-			} else {
-				console.log("email does not match a user's email");
-			}
-		}, err => console.log(err));
-		// ugly casting is ugly
+			// get user id for toUser
+			let emailMatch: boolean = false;
+			this.usersService.searchUserByEmail(""+this.fromUser)
+			.subscribe(x => {
+				console.log(x);
+				if (x[0].email == this.fromUser) {
+					this.newLoan.from = x[0].id;
+					this.loansService.postNewLoan(+this.newLoan.from, +this.newLoan.to, 
+						this.newLoan.amountCents, +this.newLoan.category)
+						.subscribe(x => this.cancel(), x => console.log(x));
+				} else {
+					console.error("email does not match a user's email");
+				}
+			}, err => console.error(err));
+			// ugly casting is ugly
+		} else {
+			console.error("invalid currency string");
+		}
 	}
 
 	cancel() {
 		// this.modalService.closeModal(this.enclosingModalId);
 		this.closeModal.emit();
 	}
+
+	isValidCurrenyString(amount: string): boolean {
+		let regex = /^\d+(?:\.\d{0,2})?$/;
+		return regex.test(amount);
+	}
+
+	convertToCents(amount: string): number {
+		if (amount.split('.').length == 2) {
+			return (+amount.split('.')[0] * 100) + (+amount.split('.')[1].substring(0,2));
+		} else if (amount.split('.').length == 1) {
+			return +amount.split('.')[0] * 100;
+		}
+		return -1;	
+	}
+
 }
--- src/public/app/lend-form/lend-form.component.ts
+++ src/public/app/lend-form/lend-form.component.ts
@@ -26,7 +26,7 @@ export class LendFormComponent implements OnInit {
 		// {name:'Transport', identifier: 'category-transport', id: 4}, 
 		// {name:'Other', identifier: 'category-other', id: 5}
 	];
-	
+
 	constructor(private authService: AuthenticationService,
 				private loansService: LoansService,
 				private usersService: UsersService,
@@ -45,33 +45,52 @@ export class LendFormComponent implements OnInit {
 
 	completeLend() {
 		// convert amount string to cents - !!
-		this.newLoan.amountCents = +this.amount;
+		if (this.isValidCurrenyString(this.amount)) {
+			this.newLoan.amountCents = this.convertToCents(this.amount);
+			console.log(this.newLoan.amountCents);
 
-		this.newLoan.from = sessionStorage.user_id;
+			this.newLoan.from = sessionStorage.user_id;
 
-		// get user id for toUser
-		let emailMatch: boolean = false;
-		this.usersService.searchUserByEmail(""+this.toUser)
-		.subscribe(x => {
-			console.log(x);
-			if (x[0].email == this.toUser) {
-				this.newLoan.to = x[0].id;
-				this.loansService.postNewLoan(+this.newLoan.from, +this.newLoan.to, 
-					this.newLoan.amountCents, +this.newLoan.category)
-					.subscribe(x => this.cancel(), x => console.log(x));
-			} else {
-				console.log("email does not match a user's email");
-			}
-		}, err => {
-			console.log(err);
-		});
+			// get user id for toUser
+			let emailMatch: boolean = false;
+			this.usersService.searchUserByEmail(""+this.toUser)
+			.subscribe(x => {
+				console.log(x);
+				if (x[0].email == this.toUser) {
+					this.newLoan.to = x[0].id;
+					this.loansService.postNewLoan(+this.newLoan.from, +this.newLoan.to, 
+						this.newLoan.amountCents, +this.newLoan.category)
+						.subscribe(x => this.cancel(), x => console.log(x));
+				} else {
+					console.error("email does not match a user's email");
+				}
+			}, err => {
+				console.error(err);
+			});
+			// ugly casting is ugly
+		} else {
+			console.error("invalid currency string");
+		}
 		
-		// ugly casting is ugly
 	}
 
 	cancel() {
 		// this.modalService.closeModal(this.enclosingModalId);
 		this.closeModal.emit();
+	}
+
+	isValidCurrenyString(amount: string): boolean {
+		let regex = /^\d+(?:\.\d{0,2})?$/;
+		return regex.test(amount);
+	}
+
+	convertToCents(amount: string): number {
+		if (amount.split('.').length == 2) {
+			return (+amount.split('.')[0] * 100) + (+amount.split('.')[1].substring(0,2));
+		} else if (amount.split('.').length == 1) {
+			return +amount.split('.')[0] * 100;
+		}
+		return -1;	
 	}
 
 }
